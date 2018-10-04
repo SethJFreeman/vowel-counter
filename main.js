db03e2fdbb0a9e962e74438d100ee5cc2f8797d6
Seth J. Freeman <cyanidecylinder@gmail.com>
2018-10-04 18:19:20 -0400

    added comments & finnished the code

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function countVowels(str) {
-  var trim = str.replace(/[aeiou]/g, "");
-  $("#output").val(trim);
+  var trim = str.replace(/[^aeiou]/g, ""); // removes everything thats not a value
+  $("#output").val(trim.length + " Vowels"); // Outputs the number of vowels
 }
 
-$("#submit").on("click", function() {
-  countVowels($("#input").val());
+$("#submit").on("click", function() { // Calls function when user clicks submit button
+  countVowels($("#input").val()); // Calls countVowels function with the input value set as the expression
 });
